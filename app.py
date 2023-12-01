1d4b49b000a911226e54a5aea1b648a3c90f8477
Ebaghrar <117086855+Ebaghrar@users.noreply.github.com>
2023-12-01 16:07:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Bonjour, OpenShift !'
+    return 'Bonjour, OpenShift & RHACS !'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(debug=True)
